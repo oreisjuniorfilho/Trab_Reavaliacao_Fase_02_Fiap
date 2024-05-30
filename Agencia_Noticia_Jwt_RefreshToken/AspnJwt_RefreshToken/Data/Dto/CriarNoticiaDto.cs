716ba4d32babacb36a878b02fdb3f10638109340
oreisjuniorfilho <138736872+oreisjuniorfilho@users.noreply.github.com>
2024-05-30 14:27:46 -0300

    Adicionar arquivos de projeto.

---
+++ Agencia_Noticia_Jwt_RefreshToken/AspnJwt_RefreshToken/Data/Dto/CriarNoticiaDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcBlogNoticias.Api.Data.Dto
+{
+    public class CriarNoticiaDto
+    {          
+        public string? Genero { get; set; }
+        public DateTime Lancamento { get; set; }        
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "O campo de Titulo é obrigatório")]
+        public string? Titulo { get; set; }
+        public string? Classificacao { get; set; }
+    }
+}
